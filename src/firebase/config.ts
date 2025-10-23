7236540e0e63a429df57e8c6c4bcdb9a4093e0d7
Mustafa Diab <mustafadiab2942000@gmail.com>
2025-10-23 17:47:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6017697584-aeed8",
+  "appId": "1:218880186103:web:86c3dd1cc72ff1845465f2",
+  "apiKey": "AIzaSyD6Wd7U7DOx3FqCNrdi4wVEdE8x2hCrXH8",
+  "authDomain": "studio-6017697584-aeed8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "218880186103"
+};
